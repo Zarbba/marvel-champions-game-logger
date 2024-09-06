0805c393456c95fb5ae9bf401c39f0fec77ccbd1
Zarbba <zarbba@hotmail.com>
2024-09-06 12:55:45 +1000

    Adds configuration to server file

---
+++ server.js
@@ -0,0 +1,37 @@
+//----------------------- DB Setup
+require(`dotenv`).config()
+const mongoose = require(`mongoose`)
+const MongoStore = require(`connect-mongoose`)
+mongoose.connect(process.env.MONGODB_URI)
+
+//----------------------- Module Imports
+const morgan = require(`morgan`)
+const express = require(`express`)
+const methodOverride = require(`method-override`)
+const session = require(`express-session`)
+const app = express()
+const authController = require(`.controllers/auth`)
+const gameController = require(`./controllers/games`)
+
+//----------------------- Server Config
+app.use(morgan(`dev`))
+app.use(express.static(`public`))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.set(`view engine`, `ejs`)
+app.use(methodOverride(`_method`, {methods: [`POST`, `GET`]}))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({mongoUrl: process.env.MONGODB_URI})
+}))
+app.use((req, res, next) => {
+    res.locals.user = req.session.user ? req.session.user : null
+    next()
+})
+app.listen(process.env.PORT,() => {
+    console.log(`Server listening at http://localhost:${process.env.PORT}/`)
+})
+
+//----------------------- Routing
