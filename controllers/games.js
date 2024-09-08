a76128769e8968e6cb21cd5447fd672f2db491d1
Zarbba <zarbba@hotmail.com>
2024-09-08 16:20:50 +1000

    Adds a check for duplicate names when logging games. Refactors handling of add player click.

--- controllers/games.js
+++ controllers/games.js
@@ -35,7 +35,20 @@ router.get(`/new`, isLoggedIn, (req, res) => {
 
 router.post(`/`, isLoggedIn, async (req, res) => {
     try {
-        //TODO - Add check for duplicate game names.
+        const nameInDatabase = await Game.find({gameName: req.body.gameName})
+        if(nameInDatabase !== null) {
+            const game = {
+                gameName: req.body.gameName,
+                datePlayed: req.body.datePlayed,
+                scenario: req.body.scenario,
+                players: generatePlayers(req.body),
+                wonGame: req.body.wonGame,
+                notes: req.body.notes,
+                owner: req.session.user    
+            }
+            res.render(`games/new`, {game, message: `A game log already exists with that name.`})
+            return
+        }
         const createdGame = await Game.create({
             gameName: req.body.gameName,
             datePlayed: req.body.datePlayed,
@@ -84,7 +97,21 @@ router.get(`/:gameId/edit`, isLoggedIn, isGameOwner, async (req, res) => {
 
 router.put(`/:gameId`, isLoggedIn, isGameOwner, async (req, res) => {
     try {
-        //TODO - Add check for duplicate game names.
+        const nameInDatabase = await Game.find({gameName: req.body.gameName})
+        const targetGame = await Game.findById(req.params.gameId)
+        if (nameInDatabase._id !== targetGame._id) {
+            const game = {
+                gameName: req.body.gameName,
+                datePlayed: req.body.datePlayed,
+                scenario: req.body.scenario,
+                players: generatePlayers(req.body),
+                wonGame: req.body.wonGame,
+                notes: req.body.notes,
+                owner: req.session.user    
+            }
+            res.render(`games/edit`, {game, message: `A game log already exists with that name.`})
+            return
+        }
         const updatedGame = await Game.findByIdAndUpdate(
             req.params.gameId, 
             {
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 // -----Constants-----
 
 // -----Cached DOM Elements-----
-const addPlayerEl = document.querySelector(`.add-player`)
+const addPlayerEl = document.querySelector(`.add`)
+
 const playerSectionEl = document.querySelector(`.player-section`)
 const coutnerEl = document.querySelector(`.counter`)
 
@@ -9,26 +10,30 @@ const coutnerEl = document.querySelector(`.counter`)
 let playerCounter = coutnerEl ? Number(coutnerEl.textContent) : 0
 
 // -----Event Listeners-----
-if (addPlayerEl) {
-    addPlayerEl.addEventListener(`click`, (e) => {
-        handleClick(e)
-    })
-}
+document.addEventListener(`click`, (e) => {
+    if (e.target.classList.contains(`player-button`))
+    handleClick(e)
+})
 
 // -----Functions-----
 function handleClick(e) {
-    if (playerCounter < 3) {
-        console.log(playerCounter)
+    e.target.classList.contains(`add`) ? addPlayer() : removePlayer()
+}
+
+function addPlayer() {
+    if (playerCounter < 4) {
         playerSectionEl.innerHTML +=`<label for="player-name${playerCounter}">Player Name:</label>
         <input id="player-name${playerCounter}" type="text" name="playerName" required>
         <label for="hero${playerCounter}">Hero:</label>
-        <input id="hero${playerCounter}" type="text" name="playerHero" required>`
+        <input id="hero${playerCounter}" type="text" name="playerHero" required>
+        <button type="button">Remove Player</button>`
         playerCounter++
-    } else {
-        playerSectionEl.innerHTML +=`<label for="player-name${playerCounter}">Player Name:</label>
-        <input id="player-name${playerCounter}" type="text" name="playerName" required>
-        <label for="hero${playerCounter}">Hero:</label>
-        <input id="hero${playerCounter}" type="text" name="playerHero" required>`
-        addPlayerEl.classList.add(`hidden`)
+        if (playerCounter >= 4) {
+            addPlayerEl.classList.add(`hidden`)
+        }
     }
+}
+
+function removePlayer() {
+    //TODO - Figure out logic for this function
 }
